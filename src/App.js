ef73832d2bcc9a5270a09639f5cc09f65e8e4a53
Anthony Boffman <amarrokalboffman@gmail.com>
2020-07-06 13:01:00 -0700

    Add google login button with client ID

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Loginbygoogle from "./components/GoogleBtn";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Loginbygoogle}></Route>
+              <Route path="/Dashboard" component={Dashboard}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </>
   );
 }
 
